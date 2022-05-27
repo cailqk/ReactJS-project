e41eda0dc76865c0d12646faa467a4308bb0af3f
cailqk <tsvetan.bogdanov@abv.bg>
2022-05-27 16:53:57 +0300

    Added more logic into the filtering process

--- src/custom_components/Contents/Workouts.js
+++ src/custom_components/Contents/Workouts.js
@@ -5,28 +5,36 @@ import Filter from "../UI/Filter";
 import { useState } from "react";
 
 function Workouts(props) {
-  const [filteredMonth, setFilteredMonth] = useState("1");
+  const [filteredMonth, setFilteredMonth] = useState("0");
 
   const filterChangeHandler = (selectedMonth) => {
     setFilteredMonth(selectedMonth);
   };
 
-  const filteredWorkouts = props.item.filter(el => {
-      return el.date.getMonth().toString() === filteredMonth;
+  const filteredWorkouts = props.item.filter((el) => {
+    return el.date.getMonth().toString() === filteredMonth;
   });
+
+  let workoutsContent;
+
+  if(filteredWorkouts.length === 0) {
+    workoutsContent = <p className="empty_message">You have no logged workouts for this month.</p>;
+  } else {
+    workoutsContent = filteredWorkouts.map((el) => (
+      <WorkoutType
+        key={el.id}
+        date={el.date}
+        type={el.type}
+        distance={el.distance}
+      />
+    ))
+  }
 
   return (
     <div>
       <Shell className="train">
         <Filter onChangeFilter={filterChangeHandler} selected={filteredMonth} />
-        {filteredWorkouts.map((el) => (
-          <WorkoutType
-            key={el.id}
-            date={el.date}
-            type={el.type}
-            distance={el.distance}
-          />
-        ))}
+        {workoutsContent}
       </Shell>
     </div>
   );
